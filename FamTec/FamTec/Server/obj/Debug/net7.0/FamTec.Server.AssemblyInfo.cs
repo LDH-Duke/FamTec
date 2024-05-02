6e23246c47eaba2c05b00f63c28de4b57e64b565
duke <ehdgml506@naver.com>
2024-05-02 10:37:13 +0900

    feat : git setting test

--- FamTec/FamTec/Server/obj/Debug/net7.0/FamTec.Server.AssemblyInfo.cs
+++ FamTec/FamTec/Server/obj/Debug/net7.0/FamTec.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FamTec.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+705884f14b9d22426a4cca4d54685fcf09f0c928")]
 [assembly: System.Reflection.AssemblyProductAttribute("FamTec.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FamTec.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
