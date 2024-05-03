65247ea8812a1e89d0ee2211591b8ef6384efa20
duke <ehdgml506@naver.com>
2024-05-03 16:46:59 +0900

    feat : table update

--- FamTec/FamTec/Client/obj/Debug/net7.0/FamTec.Client.AssemblyInfo.cs
+++ FamTec/FamTec/Client/obj/Debug/net7.0/FamTec.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FamTec.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43a542a6e49aa43218f315006fc51f88d8c83dae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3062a500260649eba09ff78e9b9f80da00262bc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("FamTec.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FamTec.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- FamTec/FamTec/Shared/Client/DTO/PlaceDTO.cs
+++ FamTec/FamTec/Shared/Client/DTO/PlaceDTO.cs
@@ -8,13 +8,14 @@ namespace FamTec.Shared.Client.DTO
 {
     public class PlaceDTO
     {
+        public bool IsSelect { get; set; } = false;
         public string? 사업장코드 {  get; set; }
         public string? 사업장명 { get; set; } = null;
         public string? 비고 { get; set; } = null;
         public string? 계약번호 { get; set; } = null;
         public DateTime 계약일자 { get; set; }
         public string? 계약상태 { get; set; }
-        public bool IsSelect { get; set; } = false;
+        
 
     }
 }
