7a1451053ab888d7052309ad59a849952b0840ea
duke <ehdgml506@naver.com>
2024-05-08 16:52:13 +0900

    feat : 1차 테이블, 모달 구현 / 리스트 구현 예

--- FamTec/FamTec/Client/obj/Debug/net7.0/FamTec.Client.AssemblyInfo.cs
+++ FamTec/FamTec/Client/obj/Debug/net7.0/FamTec.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FamTec.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e0cd713dd7b9f8cc971f3f2773c26af5a53325d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+655f7ed6a698d3ea6b5927b353fae68ac1111551")]
 [assembly: System.Reflection.AssemblyProductAttribute("FamTec.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FamTec.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
