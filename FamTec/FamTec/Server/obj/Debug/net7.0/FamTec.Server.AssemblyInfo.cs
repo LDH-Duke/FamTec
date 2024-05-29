636ee1429a3a3fe02b2b79f400904670f898f4d0
duke <ehdgml506@naver.com>
2024-05-29 10:09:11 +0900

    feat : update 24-05-29

--- FamTec/FamTec/Server/obj/Debug/net7.0/FamTec.Server.AssemblyInfo.cs
+++ FamTec/FamTec/Server/obj/Debug/net7.0/FamTec.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FamTec.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8d37c032ae6c7904e27249d0c2e014729e814e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f34fbb7fb7cd14b2ed938eee3e21ac30718ba4ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("FamTec.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FamTec.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
