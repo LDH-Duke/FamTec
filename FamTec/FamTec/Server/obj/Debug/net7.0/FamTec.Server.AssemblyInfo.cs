974040b062007bf56cb032cf0bad41b07cc542a5
DUKE <dukeldh1128@gmail.com>
2024-05-29 22:03:27 +0900

    feat : 폴더 정리 완료

--- FamTec/FamTec/Server/obj/Debug/net7.0/FamTec.Server.AssemblyInfo.cs
+++ FamTec/FamTec/Server/obj/Debug/net7.0/FamTec.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FamTec.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+636ee1429a3a3fe02b2b79f400904670f898f4d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6721730ed8f046d477e3e275b9e856a3e5016d47")]
 [assembly: System.Reflection.AssemblyProductAttribute("FamTec.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FamTec.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
