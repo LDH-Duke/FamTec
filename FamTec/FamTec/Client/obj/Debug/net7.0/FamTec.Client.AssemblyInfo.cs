0e49388827595230e9385fe3dab96743ce49a9ab
duke <ehdgml506@naver.com>
2024-06-05 08:01:59 +0900

    feat : update 24-06-05 am

--- FamTec/FamTec/Client/obj/Debug/net7.0/FamTec.Client.AssemblyInfo.cs
+++ FamTec/FamTec/Client/obj/Debug/net7.0/FamTec.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FamTec.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac965c9eeb987296a14caf4b9994eacb0c330a71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4079d57c3de9a121047abbe8cf71dc05943e5f3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("FamTec.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FamTec.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
