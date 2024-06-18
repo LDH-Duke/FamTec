c907d0693c51e73f210b8059356da2e755aace7d
duke <ehdgml506@naver.com>
2024-06-18 17:04:52 +0900

    feat : update 24.06.18

--- FamTec/FamTec/Shared/Client/DTO/Normal/Voc/ListVoc.cs
+++ FamTec/FamTec/Shared/Client/DTO/Normal/Voc/ListVoc.cs
@@ -12,8 +12,11 @@ namespace FamTec.Shared.Client.DTO.Normal.Voc
         public string? Location { get; set; }
         public int Type { get; set; }
         public string? Writer { get; set; }
+        public string? Tel { get; set; }
         public string? Title { get; set; }
+        public string? Context { get; set; }
         public int Status { get; set; }
+        
         public DateTime? Occur_DT { get; set; }
         public DateTime? Compelete_DT { get; set; }
         public string? Total_DT { get; set; }
